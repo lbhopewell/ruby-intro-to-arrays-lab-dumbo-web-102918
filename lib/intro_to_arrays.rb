5c56af5f025c5f222369a8489097d35b06e9632d
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-19 11:08:49 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,22 +10,22 @@ def first_element(taylorswift)
   @taylorswift[0]
 end
 
-def third_element(@taylor_swift)
+def third_element(taylor_swift)
   @taylor_swift[2]
 end
 
-def last_element(@taylor_swift)
+def last_element(taylor_swift)
   @taylor_swift[-1] 
 end
 
-def first_element_with_array_methods(@south_east_asia)
+def first_element_with_array_methods(south_east_asia)
   @south_east_asia.first
 
-def last_element_with_array_methods(@south_east_asia)
+def last_element_with_array_methods(south_east_asia)
   @south_east_asia.last 
 end
 
-def length_of_array(@programming_languages)
+def length_of_array(programming_languages)
   @programming_languages.length
 end
 
