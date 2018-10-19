0d886d67fdd661fa76997c0f4438b767d5d041e2
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-19 11:13:15 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,7 +2,7 @@ def instantiate_new_array
   @my_new_array = []
 end
 
-def array_with_two_elements(element1,element2)
+def array_with_two_elements(my_two_array)
   @my_two_array = [element1,element2]
 end
 
