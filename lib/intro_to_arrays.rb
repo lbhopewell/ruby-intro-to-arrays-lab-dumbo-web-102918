881cb5ab366cf4568257b5aecc78870926ee9d22
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-19 10:51:23 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,7 +18,8 @@ def last_element(@taylor_swift)
   @taylor_swift[-1] 
 end
 
-def 
+def first_element_with_array_methods(@south_east_asia)
+  @south_east_asia.first
 
 def last_element_with_array_methods(@south_east_asia)
   @south_east_asia.last 
